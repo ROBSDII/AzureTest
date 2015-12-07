d6d3ab7ddcd53abd1d39471536ec04f683ee9e25
1313768 <robert_dohring@yahoo.com>
2015-12-07 12:28:41 +0000

    Sup

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1313768
+ * Date: 07/12/2015
+ * Time: 12:18
+ */
+<!DOCTYPE html>
+<html>
+    <head>
+	</head>
+	<body>
+        <p>
+          <?php
+            echo "Hello World";
+          ?>
+</p>
+</body>
+</html>
